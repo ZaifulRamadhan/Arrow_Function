6f14ce2dd8b90231a191c51d26a9cf5038fb5914
Zaiful Ramadhan <137678044+ZaifulRamadhan@users.noreply.github.com>
2023-09-17 13:50:08 +0700

    Update data-source.js
    perubahan menjadikan menggunakan async/await

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,14 +1,13 @@
-const DataSource = (onSuccess, onFailed) => {
-  this.onSuccess = onSuccess;
-  this.onFailed = onFailed;
-}
-
-DataSource.prototype.searchClub = keyword => {
-  const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-  if (filteredClubs.length) {
-    this.onSuccess(filteredClubs);
-  } else {
-    this.onFailed(`${keyword }is not found`);
+class DataSource {
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+    const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+ 
+    if (filteredClubs.length) {
+      this.onSuccess(filteredClubs);
+    } else {
+      this.onFailed(`${keyword} is not found`);
+    }
+  });
   }
-};
+}
